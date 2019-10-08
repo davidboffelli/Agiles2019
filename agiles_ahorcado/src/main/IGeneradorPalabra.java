69133c2afecfbd5d3f796875ff61decfdc62811e
David <David@10.0.23.29>
2019-10-08 19:02:03 -0300

    commit inicial

---
+++ agiles_ahorcado/src/main/IGeneradorPalabra.java
@@ -0,0 +1,12 @@
+package main;
+
+public interface IGeneradorPalabra {
+
+	String palabraFija = "";
+
+	public String getSeleccionPalabra();
+
+	public void setPalabraFija(String pal);
+
+	public String getPalabraFija();
+}
